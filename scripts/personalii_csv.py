46ce47aee8cbd772af46a59f92880f0c8d47f62a
Elena Filippova <efilippova69@gmail.com>
2020-02-14 07:24:32 +0300

    improve person detection in script personalii_csv.py

--- scripts/personalii_csv.py
+++ scripts/personalii_csv.py
@@ -16,12 +16,15 @@ morph = pymorphy2.MorphAnalyzer()
 
 sourcecol = 'персоналии' # Столбец со строками для обработки
 rescol = 'персонали_Ип' # столбец куда надо будет записать данные
+rescol_prim = 'персоналии_примечание' # Столбец для примечаний (содержимое скобок)
 
-IOdot = re.compile(r'[А-Я]\.') # регулярное выражение для определения И. О.
-EndComma = re.compile(r',$') # запятые в конце слов
-EndDot = re.compile(r'[А-Я][а-я]+[.]$') #Фамилии с точкой на конце
-FIO =  re.compile(r'[А-Я]\. [А-Я]\. [А-Я][а-я]+') #регулярное выражение для И. О. Фамилия
-HardSur = ["Черкасск", "Неизвест", "Кохановс", "Грот"] # Слова и фамилии которые должны быть обработаны, но не детектируются другими подходами
+IOdot = re.compile(r'^[А-ЯЁ]\.$') # регулярное выражение для определения И. О.
+MultiIO = re.compile(r'^[А-ЯЁ]\. [А-ЯЁ]\.,$') # регулярное выражение для определения И. О.
+EndSym = re.compile(r'[,;]$') # запятые в конце слов
+EndDot = re.compile(r'[А-ЯЁ][а-яё]+\.$') #Фамилии с точкой на конце
+FIO_iof =  re.compile(r'^([А-ЯЁ]\. )?[А-ЯЁ]\. [А-ЯЁ][а-яё]+$') #регулярное выражение для И. О. Фамилия
+FIO_fio =  re.compile(r'^[А-ЯЁ][а-яё]+ [А-ЯЁ]\.( [А-ЯЁ]\.)?$') #регулярное выражение для Фамилия И.О.
+HardSur = ["Черкасск", "Неизвест", "Кохановс", "Грот", "Огарёв"] # Слова и фамилии которые должны быть обработаны, но не детектируются другими подходами
 
 # Потенциальные теги для определения фамилий
 keys_poss_fio= ['anim','Surn','Geox','Orgn','Trad']
@@ -43,6 +46,12 @@ def test_match_hard(_word):
         if result:
             return True
 
+def safe_list_get (l, idx, default):
+    try:
+        return l[idx]
+    except IndexError:
+        return default
+
 def morph_to_ip(_cell):
     lst = _cell.split() # разделяем ячейку на отдельные слова
     indxlst = []
@@ -53,8 +62,8 @@ def morph_to_ip(_cell):
         logging.info(cur_word)
 
         # Проверяем на запятую в конце слова. Если найдена - отпиливаем. 
-        if EndComma.findall(cur_word):
-            logging.info('comma found')
+        if EndSym.findall(cur_word):
+            logging.info('end symbol found')
             word=cur_word[:-1]
         else:
             word=cur_word
@@ -92,13 +101,29 @@ def morph_to_ip(_cell):
             if num > 0:
                 indxlst.append(num-1)
             indxlst.append(num)
-        elif (num > 1) and FIO.match(lst[num-2]+lst[num-1]+lst[num]):
-        # Pymorphy не определил фамилию, но подпадает под regex
-            if num > 1:
-                indxlst.append(num-2)
-            if num > 0:
-                indxlst.append(num-1) 
+
+
+        if (num > 1):
+            last_3=" ".join([lst[num-2],lst[num-1],lst[num]])
+        if (num > 0):
+            last_2=" ".join([lst[num-1],lst[num]])
+
+        if (num > 1) and FIO_iof.match(last_3):
+            indxlst.append(num-2)
+            indxlst.append(num-1) 
             indxlst.append(num)
+
+        if (num > 0) and FIO_fio.match(last_2):
+            indxlst.append(num-1) 
+            indxlst.append(num)
+
+        if (num > 0) and (MultiIO.match(last_2)):
+            indxlst.append(num-1) 
+            indxlst.append(num)
+
+        # if (IOdot.match(lst[num])):
+        #     indxlst.append(num)
+        
         logging.info (indxlst)
     
     indxlst = sorted(set(indxlst)) # Сортируем по возрастанию, убираем повторяющиеся номера
@@ -106,55 +131,60 @@ def morph_to_ip(_cell):
 
     # Начинаем выдергивать слова по номерам, приводить их в именительный падеж единственное число
     for num in indxlst:
-        cur_word=lst[num] #Получаем слово
-
+        word=lst[num] #Получаем слово
+        
         # Проверяем на запятую в конце слова. Если найдена - отпиливаем. И запоминаем, чтобы вернуть в конце
-        if EndComma.findall(cur_word):
-            HasEndComma=True
-            word=cur_word[:-1]
-        else:
-            HasEndComma=False
-            word=cur_word
+        HasEndSym=False
+        if EndSym.findall(word):
+            HasEndSym=True
+            EndSymSave=word[-1]
+            word=word[:-1]
 
         # Отпиливаем точку
-        if EndDot.findall(word):
+        HasEndDot=False
+        if word.endswith('.'):
+            HasEndDot=True
             word=word[:-1]
 
         logging.info(word)
-        parsed = morph.parse(word)  # вызываем разбор слова в pymorphy
-        p=None
 
-        for variant in parsed:
-            # Смотрим варианты разбора, определяем более подходящий
-            # тот, у которого будет хотя бы один из заданных тегов
-            # и дательный падеж
-            if test_tag_list(['datv'],variant.tag) and test_tag_list(keys_poss_fio, variant.tag):
-                p=variant
-                break
-        # Если такого не найдено - берем первый вариант.
-        if not p:
-            p=parsed[0]
+        if len(word)>1:
+            parsed = morph.parse(word)  # вызываем разбор слова в pymorphy
+            p=None
 
-        logging.info(p.tag)
-        if test_tag_list(keys_poss_fio, p.tag) or test_match_hard(word):
-        # Смотрим что это слово или сложное для детектирования слово
-            if p.inflect({'sing','nomn'}):
-                res=p.inflect({'sing','nomn'}).word # приводим слово в И.П. единственное число
-            else:
-                res=""
-            res=res.title()
+            for variant in parsed:
+                # Смотрим варианты разбора, определяем более подходящий
+                # тот, у которого будет хотя бы один из заданных тегов
+                # и дательный падеж
+                if test_tag_list(['datv'],variant.tag) and test_tag_list(keys_poss_fio, variant.tag):
+                    p=variant
+                    break
+            # Если такого не найдено - берем первый вариант.
+            if not p:
+                p=parsed[0]
 
-            if HasEndComma: # если была запятая - возвращаем
-                res=res+','
+            logging.info(p.tag)
+            #if test_tag_list(keys_poss_fio, p.tag) or test_match_hard(word):
 
-            words.append(res) # добавляем в результирующий список
-            logging.info('final word status: '+res)
-        else:
-        # Не фамилия - оставляем без обработки. Только запятую возвращаем если была
-            if HasEndComma:
-                words.append(word+',')
+            # Смотрим что это слово или сложное для детектирования слово
+            #if p.inflect({'sing','nomn'}):
+            #    res=p.inflect({'sing','nomn'}).word # приводим слово в И.П. единственное число
+            if p.inflect({'nomn'}):
+                res=p.inflect({'nomn'}).word # приводим слово в И.П. (число не меняем)
+                #    res=""
+                #if  test_tag_list(keys_poss_fio, p.tag) or test_match_hard(word):
+                res=res.title()
+
+                if HasEndSym: # если была запятая - возвращаем
+                    res=res+EndSymSave
+
+                words.append(res) # добавляем в результирующий список
+                logging.info('final word status: '+res)
             else:
                 words.append(word)
+        else:
+        # Не фамилия - оставляем без обработки
+            words.append(lst[num])
 
     return ' '.join(words) #Возвращаем в качестве результата найденные слова в И.П. единственное число
 
