fef05ec648c5dcd6f6f7513f7e27393ff732d1a7
Elena Filippova <efilippova69@gmail.com>
2020-02-23 13:11:02 +0300

    Update 2_regex_csv_split_adresat.py
    refine/cleanup script 2

--- scripts/2_regex_csv_split_adresat.py
+++ scripts/2_regex_csv_split_adresat.py
@@ -1,29 +1,15 @@
 # Зависимости
 # Python 3.8 https://www.python.org/downloads/
-# pip install pymorphy2
-# pip install -U pymorphy2-dicts-ru
 
-import pymorphy2
-from pymorphy2.shapes import restore_capitalization
 import re
-import logging
 import csv
 import argparse
-
-# Логирование в utf-8 для отладки 
-logging.basicConfig(handlers=[logging.FileHandler(r"xlwings.log", 'w', 'utf-8')], level=logging.INFO)
 
 sourcecol = 'адресат' # Столбец со строками для обработки
 rescols = ['адресат_имя', 'адресат_примечание'] # столбцы куда надо будет записать данные
 
-def safe_list_get (l, idx, default):
-    try:
-        return l[idx]
-    except IndexError:
-        return default
-
 def extract_data_person(_celldata):
-    res_cell=re.search(r'^(.*?)( ?[\(\[].*[\]\)])?$',_celldata, re.IGNORECASE)
+    res_cell=re.search(r'^(.*?)([\(\[].*[\]\)])?$',_celldata, re.IGNORECASE)
     if res_cell:
         person=res_cell.group(1)
         prim=res_cell.group(2)
@@ -32,13 +18,16 @@ def extract_data_person(_celldata):
             person=person.strip()
             if person.endswith('.'):
                 person=person[:-1]
-            person=re.sub(r"\s\s+", " ", person)
+            
         if prim:
             prim=prim.strip()
-    else:
-        return ["","",""]
 
-    return [person,prim]
+        return { rescols[0]: person, 
+                rescols[1]: prim}
+    else:
+        return { rescols[0]:"", 
+                rescols[1]: ""}
+
 
 def main():
 
@@ -62,13 +51,14 @@ def main():
             if cell: 
                 extracted_data=extract_data_person(cell)
             else:
-                extracted_data=["","",""]
-            res_line=line
-            for num,col in enumerate(rescols, start=0):
-                res_line[col]=extracted_data[num]
+                extracted_data={ rescols[0]:"", 
+                rescols[1]: ""}
+
+            line.update(extracted_data)
+
             with open(outfile, "a", newline='', encoding='utf-8-sig') as resfile:
                 writer = csv.DictWriter(resfile, fieldnames=res_fieldnames, delimiter=';')
-                writer.writerow(res_line)
+                writer.writerow(line)
 
 
 if __name__ == '__main__':
