e26421fe2062d6f473d9898376dfb51e8b8074a4
Elena Filippova <efilippova69@gmail.com>
2020-02-23 13:10:40 +0300

    Update 1_regex_personalii_csv.py
    refine/cleanup script 1

--- scripts/1_regex_personalii_csv.py
+++ scripts/1_regex_personalii_csv.py
@@ -1,7 +1,5 @@
 # Зависимости
 # Python 3.8 https://www.python.org/downloads/
-# pip install pymorphy2
-# pip install -U pymorphy2-dicts-ru русский словарь
 
 import re
 import csv
@@ -18,31 +16,22 @@ def extract_data(_celldata):
     r - оставляет строку без обработки
     ^ - начало строки 
     .* - любое количество любых символов
+    re.IGNORECASE - флаг, игнорирование регистра
     '''
     if res_cell:
-        person=res_cell.group(1)
-        letters_count=res_cell.group(2)
-        dates=res_cell.group(3)
-
-        if person:
-            person=person.strip()
-            if person.endswith('.'):
-                person=person[:-1]
-            person=re.sub(r"\s\s+", " ", person)
-        if dates:
-            dates=dates.strip()
-            if re.findall(r'[.,;]$',dates):
-                dates=dates[:-1]
-            dates=re.sub(r"\s\s+", " ", dates)
+        return { rescols[0]: res_cell.group(1), 
+                rescols[1]: res_cell.group(2),
+                rescols[2]: res_cell.group(3) }
     else:
-        return ["","",""]
+        return { rescols[0]:"", 
+                rescols[1]: "",
+                rescols[2]: "" }
 
-    return [person,letters_count,dates]
 
 def main():
 
-    parser = argparse.ArgumentParser(prog='Personalii convert', description='Extract personalies into another column and/or modify them')
-    parser.add_argument('infile',  type=argparse.FileType('r', encoding='utf-8-sig'), nargs='?',
+    parser = argparse.ArgumentParser(prog='separation of personalities', description='Extract personalities into another column and/or modify them')
+    parser.add_argument('infile',  type =argparse.FileType('r', encoding='utf-8-sig'), nargs='?',
                     help='csv file for processing', default="../data/muratova.csv")
     parser.add_argument('outfile', type=argparse.FileType('w', encoding='utf-8-sig'), nargs='?',
                     help='csv file for output', default="../data/muratova_res.csv")
@@ -50,7 +39,8 @@ def main():
     infile=args.infile.name
     outfile=args.outfile.name
 
-    with open(infile, newline='', encoding='utf-8-sig') as datafile:
+    with open(infile, newline='', encoding='utf-8-sig') as datafile: 
+        # newline='' - для корректного определения новой строки
         reader = csv.DictReader(datafile, delimiter=';')
         res_fieldnames=reader.fieldnames+rescols
         with open(outfile, "w", newline='', encoding='utf-8-sig') as resfile:
@@ -61,13 +51,15 @@ def main():
             if cell: 
                 extracted_data=extract_data(cell)
             else:
-                extracted_data=["","",""]
-            res_line=line
-            for num,col in enumerate(rescols, start=0):
-                res_line[col]=extracted_data[num]
+                extracted_data={ rescols[0]:"", 
+                rescols[1]: "",
+                rescols[2]: "" }
+
+            line.update(extracted_data)
+
             with open(outfile, "a", newline='', encoding='utf-8-sig') as resfile:
                 writer = csv.DictWriter(resfile, fieldnames=res_fieldnames, delimiter=';')
-                writer.writerow(res_line)
+                writer.writerow(line)
 
 
 if __name__ == '__main__':
